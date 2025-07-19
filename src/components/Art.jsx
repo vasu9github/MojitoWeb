eba8d7777d3a9658e5aa46c8f4f34fd8840cf0cb
Vasu <vasudhiman105@gmail.com>
2025-07-19 10:55:14 +0530

    implemented menu section and fixed some issues

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -25,7 +25,7 @@ const Art = () => {
             .to('.masked-img' , { scale:1.3 , maskPosition: "center", maskSize: "400%" , duration: 1 , ease:"power1.inOut" })
             .to('#masked-content' , { opacity : 1 , duration: 1 , ease:'power1.inOut'})
         
-    } ,[])
+    })
 
 
  return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import { navLinks } from '../constants/index.js'
+import gsap from 'gsap';
 import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
+
+import { navLinks } from '../constants/index.js'
 
 const Navbar = () => {
-
-    useGSAP(() => {
-        const navTween = gsap.timeline({
-            scrollTrigger:{
-                trigger:'nav',
-                start:"bottom top"
-            }
-        });
-        navTween.fromTo('nav' , {backgroundColor: 'transparent'} , {
-            backgroundColor : '#00000050',
-            backgroundFilter: 'blur(10px)',
-            duration:1,
-            ease:"power1.inOut"
-        });
-    },[])
-
-   return (
+ useGSAP(() => {
+	const navTween = gsap.timeline({
+	 scrollTrigger: {
+		trigger: 'nav',
+		start: 'bottom top'
+	 }
+	});
+	
+	navTween.fromTo('nav', { backgroundColor: 'transparent' }, {
+	 backgroundColor: '#00000050',
+	 backgroundFilter: 'blur(10px)',
+	 duration: 1,
+	 ease: 'power1.inOut'
+	});
+ })
+ 
+ return (
 	<nav>
-	 <div className=''>
+	 <div>
 		<a href="#home" className="flex items-center gap-2">
 		 <img src="/images/logo.png" alt="logo" />
 		 <p>Vasu's Bistro</p>
@@ -39,5 +39,4 @@ const Navbar = () => {
 	</nav>
  )
 }
-
 export default Navbar
